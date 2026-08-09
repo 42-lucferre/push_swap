649b59b9d9fbeea53aead63d2b652ea97901f7c0
Julio Correa <jcorrea@student.42.fr>
2026-08-09 12:20:43 -0300

    medium_sort - work in progress

--- medium_sort.c
+++ medium_sort.c
@@ -6,7 +6,7 @@
 /*   By: jcorrea <jcorrea@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/08 11:48:40 by jcorrea           #+#    #+#             */
-/*   Updated: 2026/08/09 11:39:15 by jcorrea          ###   ########.fr       */
+/*   Updated: 2026/08/09 12:15:55 by jcorrea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,4 +76,32 @@ static void	better_rotation(t_master *master, int top_dist, int bottom_dist)
 			i--;
 		}
 	}
+}
+
+int	medium_sort(t_master *master)
+{
+	int	chunk_size;
+	int	start;
+	int	top_dist;
+	int	bottom_dist;
+
+	if ((rank_stack(master->stack_a, master->size_a)) == -1)
+		return (-1);
+	chunk_size = calc_chunk_size(master->size_a);
+	start = 0;
+	while (master->size_a > 0)
+	{
+		top_dist = top_distance(master->stack_a,
+				master->size_a, start, (start + chunk_size));
+		bottom_dist = bottom_distance(master->stack_a,
+				master->size_a, start, (start + chunk_size));
+		if (top_dist == -1 && bottom_dist == -1)
+			start += chunk_size;
+		else
+		{
+			better_rotation(master, top_dist, bottom_dist);
+			pb_operation(master);
+		}
+	}
+	return (0);
 }
